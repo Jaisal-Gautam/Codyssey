04a2c847f5c064762849b2e6e2bcb415e1db0b85
Jaisal-Gautam <jaisalwork13@gmail.com>
2025-07-13 22:06:23 +0530

    Added Sec-1 and Header Animations

--- src/module/particle.js
+++ src/module/particle.js
@@ -1,6 +1,6 @@
 window.particlesJS("particles-js", {
     particles: {
-      number: { value: 125, density: { enable: false, value_area: 800 } },
+      number: { value: 50, density: { false: true, value_area: 800 } },
       color: { value: "#000" },
       shape: {
         type: "circle",
@@ -8,13 +8,13 @@ window.particlesJS("particles-js", {
         polygon: { nb_sides: 3 },
       },
       opacity: {
-        value: 0.5,
+        value: 0.6,
         random: false,
         anim: { enable: false, speed: 1, opacity_min: 0.1, sync: false },
       },
       size: {
         value: 3,
-        random: true,
+        random: false,
         anim: { enable: false, speed: 5, size_min: 0.1, sync: false },
       },
       line_linked: {
@@ -27,7 +27,7 @@ window.particlesJS("particles-js", {
       move: {
         enable: true,
         speed: 2,
-        direction: "none",
+        direction: "right",
         random: false,
         straight: false,
         out_mode: "out",
@@ -38,15 +38,15 @@ window.particlesJS("particles-js", {
     interactivity: {
       detect_on: "window",
       events: {
-        onhover: { enable: false, mode: "grab" },
+        onhover: { enable: false, mode: "push" },
         onclick: { enable: false, mode: "push" },
-        resize: false,
+        resize: true,
       },
       modes: {
         grab: { distance: 400, line_linked: { opacity: 1 } },
         bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
         repulse: { distance: 200, duration: 0.4 },
-        push: { quantity: 4 },
+        push: { quantity: 20 },
         remove: { quantity: 2 },
       },
     },
